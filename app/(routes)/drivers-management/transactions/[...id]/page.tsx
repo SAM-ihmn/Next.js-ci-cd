068991c06f29fc445a90a948ad590ad97fed6344
Nahid <110118856+Nahid-ghorbani@users.noreply.github.com>
2023-11-25 15:55:30 +0330

    Update page.tsx

--- app/(routes)/drivers-management/transactions/[...id]/page.tsx
+++ app/(routes)/drivers-management/transactions/[...id]/page.tsx
@@ -4,7 +4,7 @@ import DriverTransactionsRows from '@/app/_components/table/TableRow/DriverTrans
 import SearchBox from '@/app/_components/tool-bar/SearchBox'
 import ToolBar from '@/app/_components/tool-bar/ToolBar'
 import React from 'react'
-import { DriversData, DriverDataType } from "../../../../../_components/_data/data";
+import { DriversData } from '@/app/_components/_data/data';
 
 type Props = {
   params: {
