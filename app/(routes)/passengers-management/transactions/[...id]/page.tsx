fdb9986167669354673e588142efeb5a1d86bf74
Nahid <110118856+Nahid-ghorbani@users.noreply.github.com>
2023-11-25 15:40:01 +0330

    Update page.tsx

--- app/(routes)/passengers-management/transactions/[...id]/page.tsx
+++ app/(routes)/passengers-management/transactions/[...id]/page.tsx
@@ -1,4 +1,5 @@
 import { passengerTransactionsTableHeader } from "@/app/_components/_data/drivers";
+import { PassengersData } from "@/app/_components/_data/data";
 import Table from "@/app/_components/table/Table";
 import DriverTransactionsRows from "@/app/_components/table/TableRow/DriverTransactionsRows";
 import PassengerTransactionsRows from "@/app/_components/table/TableRow/PassengerTransactionRow";
@@ -8,7 +9,13 @@ import React from "react";
 
 type Props = {};
 
+export function generateStaticParams() {
+  //define params
+  return PassengersData.map ((passenger) => {id : driver.code})
+}
+
 const page = ({ params }: { params: { id: number } }) => {
+  const {id} = params;
   return (
     <div className="fullSize flex flex-col gap-2">
       <ToolBar title="تراکنش های مسافر" hidden={true} hiddenNewTicket={true}>
